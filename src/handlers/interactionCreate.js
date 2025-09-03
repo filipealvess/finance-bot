991e0c8cb6fe6a2e3a0e732e03d6a7521b1b864e
Filipe Alves <63798776+filipealvess@users.noreply.github.com>
2025-09-03 18:46:27 +0000

    Add function to reduce command data

--- src/handlers/interactionCreate.js
+++ src/handlers/interactionCreate.js
@@ -1,23 +1,36 @@
-async function handleIncome(interaction) {
+function reduce(interaction) {
     const value = interaction.options.getNumber('valor');
     const category = interaction.options.getString('categoria');
 
-    console.log(`[INFO] Saved income "${value.toFixed(2)}" in category "${category}"`);
+    return {
+        category: category.toLowerCase(),
+        createdAt: new Date().toISOString(),
+        id: interaction.id,
+        type: interaction.commandName,
+        user: interaction.member.user.username,
+        value: value.toFixed(2),
+    };
+}
+
+async function handleIncome(interaction) {
+    const data = reduce(interaction);
+
+    console.log(`[INFO] Income stored`);
+    console.log(data);
 
     await interaction.reply({
-        content: `Receita de **R$ ${value.toFixed(2)}** com **${category}** registrada com sucesso!`,
+        content: `Receita de **R$ ${data.value}** com **${data.category}** registrada com sucesso!`,
     });
 }
 
 async function handleExpense(interaction) {
-    const value = interaction.options.getNumber('valor');
-    const category = interaction.options.getString('categoria');
+    const data = reduce(interaction);
 
-    console.log(`[INFO] Saved expense "${value.toFixed(2)}" in category "${category}"`);
+    console.log(`[INFO] Expense stored`);
+    console.log(data);
 
     await interaction.reply({
-        content: `Despesa de **R$ ${value.toFixed(2)}** com **${category}** registrada com sucesso!`,
-        flags: [],
+        content: `Despesa de **R$ ${data.value}** com **${data.category}** registrada com sucesso!`,
     });
 }
 
